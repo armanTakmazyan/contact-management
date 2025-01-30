b57a78ad49661582c033df2cfed0c162ed94a226
Arman Takmazyan <armanworkmail01@gmail.com>
2025-01-30 21:13:48 +0400

    Add TanStack packages and configure TanStack Route

---
+++ src/routes/index.lazy.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+import { createLazyFileRoute } from '@tanstack/react-router';
+
+const Index: FC = () => {
+  return (
+    <div className="p-2">
+      <h3>Welcome Home!</h3>
+    </div>
+  );
+};
+
+export const Route = createLazyFileRoute('/')({
+  component: Index,
+});
