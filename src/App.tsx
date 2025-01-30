0f6baeb58c1336940963e18a9e71a7ba1fc77745
Arman Takmazyan <armanworkmail01@gmail.com>
2025-01-30 21:21:03 +0400

    Add Tailwind CSS configurations

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
 // import reactLogo from './assets/react.svg';
 // import viteLogo from '/vite.svg';
-import './App.css';
 
 const router = createRouter({ routeTree });
 
